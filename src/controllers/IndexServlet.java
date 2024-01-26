2f4ac77795775e167b1eeac1f2af9de45c414924
na-tsuchihashi <na-tsuchihashi@apptec.co.jp>
2024-01-26 09:10:17 +0900

    Index/NewServlet

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,12 +1,18 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Tasks;
+import utils.DBUtil;
 
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
@@ -18,7 +24,16 @@ public class IndexServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Tasks> tasks = em.createNamedQuery("getAllTask", Tasks.class).getResultList();
+
+        em.close();
+        
+        request.setAttribute("tasks", tasks);
+        
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        rd.forward(request, response);
     }
 
 }
